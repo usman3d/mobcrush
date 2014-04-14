452f7aa05499ce14e90e2719841b18ae9cd828d1
James Hurley <jamesghurley@gmail.com>
2014-04-14 10:38:51 -0700

    Add gitignore

--- build/mobcrush.build/Debug-iphoneos/mobcrush.build/Objects-normal/armv7/Golomb.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/JamesHurley/dev/jgh-/github/mobcrush/videocore/system/h264/Golomb.cpp \
-  /Users/JamesHurley/dev/jgh-/github/mobcrush/videocore/system/h264/Golomb.h
--- build/mobcrush.build/Debug-iphoneos/mobcrush.build/Objects-normal/armv7/StreamSession.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/JamesHurley/dev/jgh-/github/mobcrush/videocore/stream/Apple/StreamSession.mm \
-  /Users/JamesHurley/dev/jgh-/github/mobcrush/videocore/stream/Apple/StreamSession.h \
-  /Users/JamesHurley/dev/jgh-/github/mobcrush/videocore/stream/IStreamSession.hpp
